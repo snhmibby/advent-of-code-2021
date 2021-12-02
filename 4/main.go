7ad4e9fb4d4a9e88e421b723b1cba8f3226aec65
Jur <jurjenm@yahoo.com>
2021-12-02 14:18:01 +0100

    completed day 2, yay

---
+++ 4/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type Cmd struct {
+	cmd string
+	val int
+}
+
+func croak(e error, msg string) {
+	if e != nil {
+		fmt.Fprintf(os.Stderr, "%s (%v).", msg, e)
+		os.Exit(1)
+	}
+}
+
+func getInput() []Cmd {
+	file, err := os.Open("input")
+	croak(err, "Cannot open file 'input'")
+	scanner := bufio.NewScanner(file)
+	var cmd []Cmd
+	for scanner.Scan() {
+		var c Cmd
+		n, _ := fmt.Sscanf(scanner.Text(), "%s %d", &c.cmd, &c.val)
+		if 2 == n {
+			cmd = append(cmd, c)
+		}
+	}
+	return cmd
+}
+
+func main() {
+	in := getInput()
+	var aim, pos, depth int
+	for _, c := range in {
+		switch c.cmd {
+		case "forward":
+			pos += c.val
+			depth += aim * c.val
+		case "down":
+			aim += c.val
+		case "up":
+			aim -= c.val
+		}
+	}
+	fmt.Print(pos * depth)
+}
