44c99e7cf53832e6d876872fbd41acbf36d97928
Jur <jurjenm@yahoo.com>
2021-12-05 06:08:28 +0100

    Better naming

--- 4.1/main.go
+++ 4.1/main.go
@@ -9,7 +9,9 @@ import (
 	"strings"
 )
 
-type Card [5][5]int
+const N = 5
+
+type Card [N][N]int
 
 func croak(e error, msg string) {
 	if e != nil {
@@ -19,8 +21,8 @@ func croak(e error, msg string) {
 
 func (c *Card) Score(marked []bool, last int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		for j := 0; j < 5; j++ {
+	for i := 0; i < N; i++ {
+		for j := 0; j < N; j++ {
 			if !marked[c[i][j]] {
 				sum += c[i][j]
 			}
@@ -31,9 +33,9 @@ func (c *Card) Score(marked []bool, last int) int {
 
 func (c *Card) Bingo(marked []bool) bool {
 	//check rows
-	for i := 0; i < 5; i++ {
+	for i := 0; i < N; i++ {
 		bingo := true
-		for j := 0; j < 5; j++ {
+		for j := 0; j < N; j++ {
 			bingo = bingo && marked[c[i][j]]
 		}
 		if bingo {
@@ -42,9 +44,9 @@ func (c *Card) Bingo(marked []bool) bool {
 	}
 
 	//check columns
-	for i := 0; i < 5; i++ {
+	for i := 0; i < N; i++ {
 		bingo := true
-		for j := 0; j < 5; j++ {
+		for j := 0; j < N; j++ {
 			bingo = bingo && marked[c[j][i]]
 		}
 		if bingo {
@@ -55,7 +57,7 @@ func (c *Card) Bingo(marked []bool) bool {
 }
 
 func (c *Card) Dump() {
-	for i := 0; i < 5; i++ {
+	for i := 0; i < N; i++ {
 		fmt.Println(c[i])
 	}
 }
@@ -88,9 +90,9 @@ func getNumbers(s string) []int {
 
 //it doesn't work without the pointers...
 //Apparently arrays are also call by value??
-func scanLine(a *[5]int, s string) bool {
+func scanLine(a *[N]int, s string) bool {
 	n, _ := fmt.Sscanf(s, "%d %d %d %d %d", &a[0], &a[1], &a[2], &a[3], &a[4])
-	return n == 5
+	return n == N
 }
 
 func main() {
@@ -109,7 +111,7 @@ func main() {
 	for scanner.Scan() {
 		if scanLine(&card[row], scanner.Text()) {
 			row++
-			if row == 5 {
+			if row == N {
 				score, num := card.Check(numbers)
 				if num < bestnum {
 					bestnum = num
