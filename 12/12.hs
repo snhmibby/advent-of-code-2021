71f8f9eb7b76d3a158c00ad2687c363384b3f722
Jur <jurjenm@yahoo.com>
2021-12-12 22:43:46 +0100

    improved graph building code

--- 12/12.hs
+++ 12/12.hs
@@ -2,11 +2,11 @@ module Main where
 
 import Control.Monad
 import Control.Monad.Trans.Writer
-import Data.Maybe
-import Data.Functor
 import Data.Char
-import Data.Array
+import Data.Functor
 import Data.List.Split
+import Data.Maybe
+import Data.Map.Strict ((!))
 import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
 
@@ -25,26 +25,12 @@ readEdge s = case splitOn "-" s of
       [to, from] -> (to, from)
       _ -> error $ "bad edge in input: " ++ s
 
-readNodes :: [Edge] -> Set.Set Node
-readNodes = Set.fromList . foldl (\b (t,f)-> t:f:b) []
-
-readNeighbors :: [Edge] -> Node -> [Node]
-readNeighbors e n = map neighborInEdge $ filter hasNode e
+mkGraph = foldr addEdge Map.empty
   where
-    neighborInEdge (v1, v2)
-      | v1 == n   = v2
-      | otherwise = v1
-    hasNode (v1, v2) = v1 == n || v2 == n
+    addEdge (v1,v2) = Map.insertWith (++) v1 [v2] . Map.insertWith (++) v2 [v1]
 
 input :: IO Graph
-input = do
-  e <- readFile "./input" <&> map readEdge .lines
-  let v = readNodes e
-  let g = Map.fromSet (readNeighbors e) v
-  return g
-
-neighbours :: Graph -> Node -> [Node]
-neighbours = (Map.!)
+input = readFile "./input" <&> mkGraph . map readEdge . lines
 
 addNode :: Seen -> Node -> Seen
 addNode s n =
@@ -56,7 +42,7 @@ walk :: Graph -> Seen -> Node -> Path -> Writer [Path] ()
 walk g s n p
   | n == "end"     = tell [p]
   | Set.member n s = return ()
-  | otherwise      = forM_ (neighbours g n) $ \v -> walk g (addNode s n) v (n:p)
+  | otherwise      = forM_ (g!n) $ \v -> walk g (addNode s n) v (n:p)
 
 paths g = execWriter $ walk g Set.empty "start" []
 
