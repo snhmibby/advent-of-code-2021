08178e65fcdd53c4ad708297006eb21156d7ce99
Jur <jurjenm@yahoo.com>
2021-12-14 16:29:47 +0100

    part 2

--- 14/14.hs
+++ 14/14.hs
@@ -1,45 +1,52 @@
 module Main where
 
+import Data.Bifunctor
 import Data.Functor
 import Data.List.Split
 import Data.List
 import qualified Data.Map as Map
 
+type Count = Map.Map Char Integer
+type Freq = Map.Map String Integer
 type Rules = Map.Map String String
-  
-input :: IO (String, Rules)
+ 
+input :: IO (Freq, Rules)
 input = do 
   template:_:rules' <- readFile "./input" <&> lines
+  let freq = str2Freq template
   let rules = Map.fromList $ map (tuple . splitOn " -> ") rules'
-  return (template, rules)
+  return (freq, rules)
     where
       tuple [a,b] = (a,b)
 
-step :: String -> Rules -> String
-step s rules = loop s
+str2Freq :: String -> Freq
+str2Freq s = Map.fromListWith (+) $ zip (zipWith (\a b -> [a,b]) s (tail s)) (repeat 1)
+
+step :: Rules -> Freq -> Freq
+step rules = Map.fromListWith (+) . concatMap addFreq . Map.assocs
   where
-    loop :: String -> String
-    loop [] = []
-    loop [a] = [a]
-    loop (a:b:cs)
-      = let nn = [a,b] in
-        case Map.lookup nn rules of
-          Just n  ->  a : n ++ loop (b:cs)
-          Nothing -> a : loop (b:cs)
+    addFreq :: (String, Integer) -> [(String, Integer)]
+    addFreq (k@[a,b], n) = case Map.lookup k rules of
+      Nothing  -> [(k,n)]
+      Just [c] -> [([a,c], n), ([c,b], n)]
 
-stepN n s r = iterate (`step` r) s !! n
+step' r = iterate (step r)
 
-count :: String -> Map.Map Char Int
-count = foldl' (\m c -> Map.insertWith (const $ (+)1) c 1 m) Map.empty
+count :: Freq -> Count
+count = Map.fromListWith (+) . map divCount . concatMap split . Map.assocs
+  where
+    divCount = second $ \x -> (x `div` 2) + (x `mod` 2)
+    split ([a,b], n) = [(a,n), (b,n)]
 
-maxOf, minOf :: Map.Map Char Int -> Int
-minOf = minimum . Map.elems
-maxOf = maximum . Map.elems
+count' :: Freq -> (Integer, Integer)
+count' f = (maximum c, minimum c)
+  where
+    c = Map.elems $ count f
 
 main :: IO ()
 main = do
   (s, rules) <- input
-  let polymer = stepN 20 s rules
-  let counts = count polymer
-  print $ maxOf counts - minOf counts
+  let part2 = step' rules s !! 40
+  let (mx, mn) = count' part2
+  print $ mx - mn
 
